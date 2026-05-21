1608d63e1f0f4a74e7ea80ccbe1cf54be4133990
lucasdbr05 <lucasgabrieljake@gmail.com>
2026-05-21 14:49:20 -0300

    feat(cmd): add common utilities for commands

---
+++ cmd/sef/commands/common.go
@@ -0,0 +1,22 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/btcsuite/btcd/wire"
+)
+
+func parseNetwork(name string) (wire.BitcoinNet, error) {
+	switch name {
+	case "mainnet":
+		return wire.MainNet, nil
+	case "testnet", "testnet3":
+		return wire.TestNet3, nil
+	case "signet":
+		return wire.BitcoinNet(0xead63049), nil
+	case "regtest":
+		return wire.TestNet, nil
+	default:
+		return 0, fmt.Errorf("unknown network %q: use mainnet, testnet, signet or regtest", name)
+	}
+}
